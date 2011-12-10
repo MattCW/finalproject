6c87bf3ea9f55209df53f8d83145d7b4a8547e98
Matthew Wann <mwann@spsu.edu>
2011-12-10 14:47:23 -0500

    Testing push/pull capabilities

---
+++ src/finalproject/testapp.scala
@@ -0,0 +1,23 @@
+package finalproject
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Matt
+ * Date: 12/10/11
+ * Time: 2:14 PM
+ * To change this template use File | Settings | File Templates.
+ * This is a test of GitHub for our team project, making everyone can pull,
+ * edit, then push code changes
+ */
+
+object testapp
+{
+  def main(args: Array[String])
+  {
+    for(i<-0 to 10)
+    {
+      println("Matt")
+    }
+  }
+
+}
