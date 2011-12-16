3e78dbd3abecef50798412d07e10ef2ca6f532d0
Matthew Wann <mwann@spsu.edu>
2011-12-16 00:20:00 -0500

    Main Actor everything except database code

---
+++ src/finalproject/Simulation.scala
@@ -0,0 +1,13 @@
+package finalproject
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Matt
+ * Date: 12/13/11
+ * Time: 2:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object Simulation {
+
+}
